7cec9501c8352ad8dab81429d2e869c4d4c5aef3
Valerio Bertone <valerio.bertone@cern.ch>
2019-11-22 18:29:24 +0100

    Updating ePDF doc

--- ePDF/html/search/all_a.js
+++ ePDF/html/search/all_a.js
@@ -5,6 +5,6 @@ var searchData=
   ['ndistributions_102',['NDistributions',['../classePDF_1_1NDistributions.html',1,'ePDF::NDistributions'],['../classePDF_1_1NDistributions.html#a01339af955ffadf139c4c33f9ff13bb1',1,'ePDF::NDistributions::NDistributions()']]],
   ['ndistributions_2eh_103',['ndistributions.h',['../ndistributions_8h.html',1,'']]],
   ['nl_104',['nl',['../structePDF_1_1int__params.html#a303c2e309f354f533059e7bd4fec9235',1,'ePDF::int_params']]],
-  ['numericintegrals_105',['NumericIntegrals',['../classePDF_1_1NumericIntegrals.html',1,'ePDF::NumericIntegrals'],['../classePDF_1_1NumericIntegrals.html#a7d564ed0869683288cf9e14aae853914',1,'ePDF::NumericIntegrals::NumericIntegrals()']]],
+  ['numericintegrals_105',['NumericIntegrals',['../classePDF_1_1NumericIntegrals.html',1,'ePDF::NumericIntegrals'],['../classePDF_1_1NumericIntegrals.html#aac973a8457c97f1152740261b9c64261',1,'ePDF::NumericIntegrals::NumericIntegrals()']]],
   ['numericintegrals_2eh_106',['numericintegrals.h',['../numericintegrals_8h.html',1,'']]]
 ];
--- ePDF/html/search/related_0.js
+++ ePDF/html/search/related_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c_334',['operator&lt;&lt;',['../classePDF_1_1Matrix.html#a72357e201ee63cf1b57d88be77b2b846',1,'ePDF::Matrix']]]
+  ['operator_3c_3c_334',['operator&lt;&lt;',['../classePDF_1_1Matrix.html#a5855ef731ee5287fab067be531028c82',1,'ePDF::Matrix']]]
 ];
