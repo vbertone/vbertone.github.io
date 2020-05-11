1a94fe87825d3923cbc1b8415652d3624a5abf0f
Valerio Bertone <valerio.bertone@cern.ch>
2020-05-11 17:21:34 +0200

    Including NNAD documentation

---
+++ NNAD/search/all_b.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['setbias_47',['SetBias',['../classnnad_1_1FeedForwardNN.html#a2e91b25841d2990ea21eff969669dbc9',1,'nnad::FeedForwardNN']]],
+  ['setelement_48',['SetElement',['../classnnad_1_1Matrix.html#a96a81ad452f088d3b75df990ab941c44',1,'nnad::Matrix']]],
+  ['setlink_49',['SetLink',['../classnnad_1_1FeedForwardNN.html#a3484a996081488defa1f642d19c1365b',1,'nnad::FeedForwardNN']]],
+  ['setparameter_50',['SetParameter',['../classnnad_1_1FeedForwardNN.html#a1e505cb3ca2a60facc8d3a4f7e5103b7',1,'nnad::FeedForwardNN::SetParameter(int const &amp;ip, T const &amp;Par)'],['../classnnad_1_1FeedForwardNN.html#a640b2b14232aaf6dc88e5962774f9d97',1,'nnad::FeedForwardNN::SetParameter(std::string const &amp;sp, T const &amp;Par)']]],
+  ['setparameters_51',['SetParameters',['../classnnad_1_1FeedForwardNN.html#a8a09c6effbfabc0a981764c3dcaa75f6',1,'nnad::FeedForwardNN']]],
+  ['sigmoid_52',['Sigmoid',['../namespacennad.html#a905ff2544e6f901d850ec1bae546d61c',1,'nnad']]]
+];
---
+++ NNAD/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['feedforwardnn_2eh_58',['FeedForwardNN.h',['../FeedForwardNN_8h.html',1,'']]]
+];
